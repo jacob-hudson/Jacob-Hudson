2a51b75f43a177a37826de60781ef73dd9b1275d
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2019-04-25 21:58:22 -0500

    running base TF

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,6 +1,36 @@
 // Configure the Google Cloud provider
 provider "google" {
  credentials = "${file("~/.gcp/creds.json")}"
- project     = "jacob-hudson.com"
- region      = "us-central"
+ project     = "jacob-hudson"
+ region      = "us-central1"
+}
+
+
+// Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+ byte_length = 8
+}
+
+// A single Google Cloud Engine instance
+resource "google_compute_instance" "default" {
+ name         = "jacob-hudson-${random_id.instance_id.hex}"
+ machine_type = "f1-micro"
+ zone         = "us-central1-a" 
+
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+ }
+
+// Make sure flask is installed on all new instances for later steps
+ metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
+
+ network_interface {
+   network = "default"
+
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
 }
